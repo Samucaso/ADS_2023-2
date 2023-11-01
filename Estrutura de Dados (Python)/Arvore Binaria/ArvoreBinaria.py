392a08c96a756945d5d0a02894a402b1fd3cde3a
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2023-10-31 21:42:01 -0300

    arvore binaria

---
+++ Estrutura de Dados (Python)/Arvore Binaria/ArvoreBinaria.py
@@ -0,0 +1,33 @@
+class NodoArvore:
+    def __init__(self, chave=None, esquerda=None, direita=None):
+        self.chave = chave
+        self.esquerda = esquerda
+        self.direita = direita
+
+    def __repr__(self):
+        return f"{self.esquerda and self.esquerda.chave} <- {self.chave} ->" \
+               f" {self.direita and self.direita.chave}"
+
+
+def em_ordem(raiz):
+    if not raiz:
+        return
+
+    em_ordem(raiz.esquerda)
+
+    print(raiz.chave),
+
+    em_ordem(raiz.direita)
+
+
+raiz = NodoArvore(40)
+raiz.esquerda = NodoArvore(20)
+raiz.direita = NodoArvore(60)
+raiz.direita.esquerda = NodoArvore(50)
+raiz.direita.direita = NodoArvore(70)
+raiz.esquerda.esquerda = NodoArvore(10)
+raiz.esquerda.direita = NodoArvore(30)
+
+print("Árvore: ", raiz)
+em_ordem(raiz)
+
