4ac017f0ae18b5a99948842d4751c952d3fafccc
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2023-11-21 21:23:53 -0300

    Commit projetos de estrutura de dados

---
+++ Estrutura de Dados (Python)/Arquivos_desafio/queue_senac.py
@@ -0,0 +1,45 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+
+# Create here the code of queue data structure SENAC
+# You have create the follow methods:
+# put() => To add a new value into the linked structure (queue)
+# get() => To recovery the element of linked structure (queue)
+
+
+# ####################################
+
+
+# start processing é o nome da rota, e vai ser acessado via metodo post
+@app.route('/start_processing', methods=['GET'])
+def start_queue_senac():
+    '''Every time you run this route, you must add a new Node to the Queue'''
+    number = random.randint(0, 9)
+    print(number, flush=True)  # Example, We need to insert the variable number in the linked structure (queue)
+
+    # You must call put() method
+    # Your code here!
+
+    return '', 200
+
+
+# rota pos processing que vai ser acessada pleo metodo post
+@app.route('/pos_processing', methods=['POST'])
+def processing_queue_senac():
+    # Every time you run this route you must retrieve the value from the queue. And run the code below!!
+    # You must call get() method
+    # Loop to empty the queue....
+    # For each value, print the value multiplied by 1000 and put a delay of 30 seconds.
+    # When the Queue is empty, inform on the screen (Print)
+    # Your code here!
+
+    print("Pos_Processing", flush=True)
+
+    return '', 200
+
+
+if __name__ == '__main__':
+    app.run(debug=__debug__)
